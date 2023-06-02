5e26257070ea01677397b3967ee93da3c059d5be
Leonardo Jorge <leonardoti4437@gmail.com>
2023-06-02 16:49:24 -0300

    feat: finish success and product page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,13 +8,14 @@ import 'keen-slider/keen-slider.min.css';
 import { HomeContainer, Product } from '../styles/pages/home';
 
 import { stripe } from '../lib/stripe';
+import Head from 'next/head';
 
 interface HomeProps {
   products: {
     id: string;
     name: string;
     imageUrl: string;
-    price: number;
+    price: string;
   }[];
 }
 
@@ -27,31 +28,38 @@ export default function Home({ products }: HomeProps) {
   });
 
   return (
-    <HomeContainer
-      ref={sliderRef}
-      className="keen-slider"
-    >
-      {products.map((product) => (
-        <Link
-          key={product.id}
-          href={`/product/${product.id}`}
-        >
-          <Product className="keen-slider__slide">
-            <Image
-              src={product.imageUrl}
-              width={520}
-              height={480}
-              alt=""
-            />
+    <>
+      <Head>
+        <title>Home | Ignite Shop</title>
+      </Head>
 
-            <footer>
-              <strong>{product.name}</strong>
-              <span>{product.price}</span>
-            </footer>
-          </Product>
-        </Link>
-      ))}
-    </HomeContainer>
+      <HomeContainer
+        ref={sliderRef}
+        className="keen-slider"
+      >
+        {products.map((product) => (
+          <Link
+            key={product.id}
+            href={`/product/${product.id}`}
+            prefetch={false}
+          >
+            <Product className="keen-slider__slide">
+              <Image
+                src={product.imageUrl}
+                width={520}
+                height={480}
+                alt=""
+              />
+
+              <footer>
+                <strong>{product.name}</strong>
+                <span>{product.price}</span>
+              </footer>
+            </Product>
+          </Link>
+        ))}
+      </HomeContainer>
+    </>
   );
 }
 
